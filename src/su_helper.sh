673c2c2e25e2498e9a153a2515537ce8675e6cb2
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-08-05 18:43:37 +0100

    Append to log not overwrite

--- src/su_helper.sh
+++ src/su_helper.sh
@@ -2,4 +2,4 @@
 # Just launch su_handler in background with no stdio to avoid errors and deadlocks
 # TODO: redirect output to a log file somewhere
 
-nohup /system/etc/nomagic/su_handler.sh "$@" >/data/adb/root.log 2>&1 </dev/null &
+nohup /system/etc/nomagic/su_handler.sh "$@" >>/data/adb/root.log 2>&1 </dev/null &
