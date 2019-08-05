19a214a7dc9b4444e72213db53c44533b9d04b81
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-08-05 18:39:42 +0100

    Fix typo

--- src/su_helper.sh
+++ src/su_helper.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 # Just launch su_handler in background with no stdio to avoid errors and deadlocks
 # TODO: redirect output to a log file somewhere
-/system/etc/nomagic/su_handler >/dev/null 2>&1 & disown
+nohup /system/etc/nomagic/su_handler.sh >/data/adb/root.log 2>&1 </dev/null &
